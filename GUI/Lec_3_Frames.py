bd2740e400f62648bca0eea2010987cdb5e297d4
Arun <arunmadhopur750@gmail.com>
2021-11-12 19:17:36 +0530

    guii updated

--- GUI/Lec_3_Frames.py
+++ GUI/Lec_3_Frames.py
@@ -2,19 +2,52 @@ from tkinter import *
 root =Tk()
 root.title('Frames Basic')
 root.geometry('400x400')
+root.resizable(0,1)
 
-#Define Frames 
-pack_frame_1 = Frame(bg='red')
-pack_frame_2 =Frame(bg='blue')
-pack_frame  =LabelFrame(text='This is label frame ')
+# Define Frames 
+# pack_frame_1 = Frame(bg='red')
+# pack_frame_2 =Frame(bg='blue')
+# pack_frame  =LabelFrame(text='This is label frame ')
 
-pack_frame_1.pack(fill=BOTH ,expand=True)
-pack_frame_2.pack(fill=BOTH ,expand=True)
-pack_frame.pack(fill=BOTH ,expand=True , padx=20 ,pady=20)
+# # pack_frame_1.pack(fill=BOTH ,expand=True ,width=500 , height =400)
+# pack_frame_1.pack(fill=BOTH ,expand=True)
+# pack_frame_2.pack(fill=BOTH ,expand=True)
+# pack_frame.pack(fill=BOTH ,expand=True , padx=20 ,pady=20) #use fill or use width and height
 
-Label(pack_frame_1 ,text='this is the first text') .pack()
+# Label(pack_frame_1 ,text='this is the first text') .pack()
 
-Label(text='This is my next text ').pack(pady=40 ,expand=True) 
+# Label(text='This is my next text ').pack(pady=40 ,expand=True) 
+
+def input():
+    # print(entry.get())
+    text  = Label(output_frame, text=entry.get())
+    text.pack()
+
+    entry.delete(0,END) #for deleting the text inside the entry after clicking on it
+
+# def count():
+#     Count=1
+#     text=Label(third_frame,ACTIVE,text=Count )
+#     Count+=1
+#     text.pack()
+
+frame =Frame(root, bg='red',width=500,height=100 ,borderwidth=5)
+frame.pack(padx=10 ,pady=10)
+entry =Entry(frame ,width=40)
+entry.grid(row=0,column=0,padx=10 ,pady=10)
+frame.grid_propagate(0)
+btn =Button(frame ,text='submit',command=input)
+btn.grid(row=0, column=1)
 
 
+output_frame =Frame(root,bg='lightgreen', width=400 ,height=100)
+output_frame.pack(padx=10 ,pady=10)
+output_frame.pack_propagate(0) #if text come at the place of bg
+
+third_frame = Frame(root, bg='green',width=500,height=300,borderwidth=3 ,relief=GROOVE)
+third_frame.pack(padx=10 ,pady=10 )
+
+# btnn = Button(frame ,text='Count',command=count)
+# btnn.grid(row=2,sticky="WE",columnspan=4,column=0)
+ 
 root.mainloop()
---
+++ GUI/Lec_6_radio.py
@@ -0,0 +1,51 @@
+from tkinter import *
+root = Tk()
+root.geometry('400x400')
+root.resizable(0, 0)
+root.title('Radio')
+
+input_frame = LabelFrame(root, width=350, height=100, text="this is fun")
+
+output_frame = Frame(root, width=350, height=100 ,bg='green')
+input_frame.pack(padx=10, pady=10)
+output_frame.pack(padx=10, pady=1)
+
+def printing_no():
+    select = 'You selected for option '+ str(number.get())  
+    label.config(text = select) #this is used so that we text does not repeat otherwise we can use label method to print 
+    
+
+# CHECKBOX
+number = IntVar()
+radio_1 = Radiobutton(input_frame,text='print the first number ', variable=number,value=1)
+radio_2 = Radiobutton(input_frame, text="print the second number ",variable=number, value=2)
+print_button = Button(input_frame, text='Print the Number' ,command=printing_no)
+
+radio_1.grid(row=0,column=0,padx=10,pady=10)
+radio_2.grid(row=0, column=1,padx=10,pady=10)
+print_button.grid(row=1, column=0, columnspan=2, pady=10, padx=10)
+
+label =Label(root)
+label.pack()
+root.mainloop()
+
+# def sel():
+#    selection = "You selected the option " + str(var.get())
+#    label.config(text = selection)
+ 
+# var = IntVar()
+# R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
+#                   command=sel)
+# R1.pack( anchor = W )
+
+# R2 = Radiobutton(root, text="Option 2", variable=var, value=2,
+#                   command=sel)
+# R2.pack( anchor = W )
+
+# R3 = Radiobutton(root, text="Option 3", variable=var, value=3,
+#                   command=sel)
+# R3.pack( anchor = W)
+
+# label = Label(root)
+# label.pack()
+# root.mainloop()
