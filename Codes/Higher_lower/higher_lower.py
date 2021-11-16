ae912cd2e58679c4900fbc5ac19be5fa901d03b7
Arun <arunmadhopur750@gmail.com>
2021-11-16 22:24:16 +0530

    codes updated

--- Codes/Higher_lower/higher_lower.py
+++ Codes/Higher_lower/higher_lower.py
@@ -1,7 +1,39 @@
 import random
-from data import data 
-choice_1= random.choice(data[0])
-choice_2=random.choice(data[0])
+import os
+from data import *
+from art import *
+flag = True 
+score=0
+while flag:
+    print(logo)
+    a = random.randint(0, 20)
+    b = random.randint(0, 20)
+    with open('z.txt','a') as file:
+         file.write(str(data[a]['follower_count']) +' ' + str(data[b]['follower_count']) )
+         file.write(' ')
+    print('Compare A: {0} , {1} from {2} '.format(data[a]['name'],data[a]['description'],data[a]['country']))
+    print(vs)
+    print('Compare B: {0} ,{1} from {2} '.format(data[b]['name'],data[b]['description'],data[b]['country']))
+    
+    choose = input('Who has more followers ? Type "a" or "b" ')
 
-print(f'Compare A: {choice_1}')
-print(f'Compare B: {choice_2}')
+    if choose == 'a':
+        if data[a]['follower_count'] > data[b]['follower_count']: #choose a 
+            score+=1
+            os.system('cls')
+            print(f'You are right! current score: {score} ')
+             
+        else:
+            flag=False
+            exit()
+    elif choose== 'b':
+        if data[b]['follower_count'] > data[a]['follower_count']: #choose a 
+            score+=1
+            os.system('cls')
+            print(f'You are right! current score: {score} ')
+             
+        else:
+            flag=False
+            exit()
+    else:
+        score+=1
