15f4a03cb648244507d5a91c8e4bb2adeaf8009e
Arun <arunmadhopur750@gmail.com>
2021-12-05 12:31:23 +0530

    functions updated

--- functions/default_arguments.py
+++ functions/default_arguments.py
@@ -1,7 +1,22 @@
+#DEFAULT ARGUMENT
 def add(data_1, data_2 , data_3=0):
     return data_1 + data_2 +data_3
 
 result1=add(2,3,3)
 result2=add(1,3)
-result3=add(data_2=4,data_1=9)
-print(result1 ,result2 ,result3)
+result3=add(data_2=4,data_1=9) #KEYWORD ARGUEMENT
+# print(result1 ,result2 ,result3)
+
+
+#VARIABLE LENGTH ARGUMENT
+def add(*n):
+    total = 0
+    for i in n:
+        total+=i 
+    print('Total is : ',total)
+    print('tuple is :' , n)
+   
+add()
+add(10)
+add(10,20)
+add(10,30)
